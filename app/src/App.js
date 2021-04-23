4e9535cbe7a461adb19b4ac03ab0c493decce1fb
chrispearson <cpearson@apterainc.com>
2021-04-23 15:24:08 -0400

    First commit

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  var sensorValue = 780;
+  var trailCondition = 'Very Muddy';
+  return (
+    <div className="App">
+      <h1>Fort Wayne Franke Park Trail Condition</h1>
+      <h2>How muddy is the trail?</h2>
+      <h4>{trailCondition}</h4>
+      <h5>{sensorValue} / 1024</h5>
+    </div>
+  );
+}
+
+export default App;

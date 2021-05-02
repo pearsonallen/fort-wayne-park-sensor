5563ff3bf6e369ce40eb82043440b51268e80466
chrispearson <cpearson@apterainc.com>
2021-05-02 13:39:39 -0400

    base64 to decimal

--- api/AddValue/index.js
+++ api/AddValue/index.js
@@ -4,11 +4,16 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = async function (context, req) {
   var tableSvc = azure.createTableService('fwiotstorage', process.env["TableStorageAccessKey"]);
   var uniqueID = uuidv4();
+
+  var t = context.req.body.uplink_message.frm_payload;
+  var t2 = new Buffer(t,'base64').toString('hex');
+  var t3 = parseInt(t2,16);
+
   var entity = {
     PartitionKey: { '_': '1' },
     RowKey: uniqueID,
     DeviceID: context.req.body.end_device_ids.device_id,
-    Value: context.req.body.uplink_message.frm_payload  //JSON.stringify(context.req.body.uplink_message.frm_payload)
+    Value: t3   //JSON.stringify(context.req.body.uplink_message.frm_payload)
   };
   tableSvc.insertEntity('SensorValues', entity, function (error, result, response) {
     if (!error) {
@@ -17,6 +22,7 @@ module.exports = async function (context, req) {
   });
   context.res = {
     // status: 200, /* Defaults to 200 */
+    
     body: "Inserted!"
   };
 }

7ca0dc7aac0dca78d38ea8e769bef52ad48436b7
Chris Pearson <pearsonland@gmail.com>
2021-07-19 17:21:19 -0400

    charts cleanup

--- app/src/SensorChart.jsx
+++ app/src/SensorChart.jsx
@@ -2,48 +2,6 @@ import React, { useEffect, useState, useRef } from "react";
 import Chart from 'chart.js/auto'
 import axios from 'axios';
 
-// function SensorChart() {
-//   const chartContainer = useRef(null);
-//   const [chartInstance, setChartInstance] = useState(null);
-
-//   useEffect(() => {
-//     if(chartContainer && chartContainer.current) {
-//       const newChartInstance = new Chart(chartContainer.current, chartConfig);
-//       setChartInstance(newChartInstance);
-//       axios.get(process.env.REACT_APP_API + "/GetHistoricalSensorValues").then(response => {
-//         updateChart(response.data);
-//       });
-//     }
-    
-//   }, [chartContainer]);
-
-//   function updateChart(data)
-//   {
-//     var ctx = document.getElementById('myChart').getContext('2d');
-
-//     var d = data.map(function(item) {return item.Value;});
-
-//     var myChart = new Chart(ctx, {
-//       type: 'line',
-//       data: {
-//           labels: null,
-//           datasets: [{
-//             label: 'My First Dataset',
-//             data: d,
-//             fill: false,
-//             borderColor: 'rgb(75, 192, 192)',
-//             tension: 0.1
-//           }]
-//         }
-//   });
-
-//   return ( <div className="container">
-//     <canvas ref={chartContainer} width="400" height="400" />
-//   </div>
-//   );
-// }
-// }
-
 function SensorChart() {
   const chartContainer = useRef(null);
   const [chartInstance, setChartInstance] = useState(null);

6754dce43b7b0a335b6b060a012c30921934aa4f
Chris Pearson <pearsonland@gmail.com>
2021-07-19 21:39:01 -0400

    sassy

--- app/src/SensorValue.jsx
+++ app/src/SensorValue.jsx
@@ -1,20 +1,18 @@
 import axios from 'axios';
 import React, { useState, useEffect } from "react";
 
-function SensorValue() {
+function SensorValue(props) {
   const [sensorValue, setSensorValue] = useState(0);
 
-
-
   useEffect(() => {
-    axios.get(process.env.REACT_APP_API + "/GetCurrentSensorValue?sensorid=frankesensor-1").then(response => {
+    axios.get(process.env.REACT_APP_API + "/GetCurrentSensorValue?sensorid=" + props.sensorID).then(response => {
       setSensorValue(response.data);
     })
   }, []);
 
-  return <div className="sensorValue">
-    The ground dryness is {sensorValue} (300 - 500 wet to dry)
-  </div>
+  return <p>
+    The wettest soil is {sensorValue} )
+  </p>
 }
 
 export default SensorValue;
